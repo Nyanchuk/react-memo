7059e5aabb0b4b377b7388dd7b814933350bdcff
global_gifts <fifa.1998.sava@mail.ru>
2023-11-30 14:21:31 +1000

    Блок баловства

--- src/pages/SelectLevelPage/SelectLevelPage.jsx
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
 import styles from "./SelectLevelPage.module.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { lightHard } from "../../store/actions & types/gameActions";
 
 export function SelectLevelPage() {
   const dispatch = useDispatch();
   const isCheckboxChecked = useSelector(state => state.game.easyMode);
+  const [showDescription, setShowDescription] = useState(false); // Состояние для отображения описания
   console.log("Состояние чекбокса:", isCheckboxChecked);
 
   function handleCheckboxChange(e) {
     dispatch(lightHard(e.target.checked));
   }
+
+  useEffect(() => {
+    if (isCheckboxChecked) {
+      setShowDescription(true); // Показываем табличку при включении упрощенного режима
+      const timeout = setTimeout(() => {
+        setShowDescription(false);
+      }, 7000);
+      return () => clearTimeout(timeout);
+    }
+  }, [isCheckboxChecked]);
 
   return (
     <div className={styles.container}>
@@ -45,6 +56,16 @@ export function SelectLevelPage() {
             Упрощенный режим
             {/* {isCheckboxChecked ? "Упрощенный режим включен" : "Упрощенный режим выключен"} */}
           </label>
+        </div>
+      </div>
+      <div>
+        <div className={`${styles.descriptionContainer} ${showDescription ? styles.descriptionSlideIn : ""}`}>
+          {showDescription && ( // Отображаем табличку при showDescription = true
+            <div className={styles.description}>
+              <p>Описание упрощенного режима:</p>
+              <p>В упрощенном режиме у вас будет больше попыток для удачного завершения игры.</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
